b81b2937761fa4e9aac55e6c72027fab6a8b5f81
logicalsofttech2023 <logicalsofttech2023@gmail.com>
2025-03-07 13:11:59 +0530

    code live

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use("/api/user", userRoutes);
 
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
