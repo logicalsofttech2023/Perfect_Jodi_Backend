1d2290c62b6628c370259cc8970b6fcb8a55701e
logicalsofttech2023 <logicalsofttech2023@gmail.com>
2025-03-11 13:38:38 +0530

    code live

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import  express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
-
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -11,9 +11,11 @@ app.use(express.json());
 app.use(cors());
 connectDB();
 
+// Make uploads folder publicly accessible
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use("/api/user", userRoutes);
-
-
 
 // Start Server
 const PORT = process.env.PORT || 5001;
